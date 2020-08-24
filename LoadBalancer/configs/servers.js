f8d47fdb486afe5ef98884df848291358d921876
kiprox <candroo@yahoo.com>
2020-08-25 06:25:58 +0700

    Initial Released Test

---
+++ LoadBalancer/configs/servers.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.ALLOW_DOMAIN 	= [];
+exports.NumberNode 		= 2; //The number of nodes you want to play
+exports.KeyCacheNode	= 'LoadBlancer';	
+exports.DomainNode 		= ['yourdomain.com']; //Your primary domain: yourdomain.com. Then you can use: sv1.yourdomain.com, sv2.yourdomain.com
+exports.Prefix			= 'sv'; //Prefix: sv1.yourdomain.com, sv2.yourdomain.com, etc.
+
---
+++ ProxyStream/configs/servers.js
@@ -0,0 +1,3 @@
+"use strict";
+exports.__esModule = true;
+exports.ALLOW_DOMAIN = ["yourdomain.com","www.jwplayer.com"]; //Domain you want to play. Put [] if you want to play on all domains.
